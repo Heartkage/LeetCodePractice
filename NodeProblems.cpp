8ac668145c16155aded9b5140e5c9ea41f44b304
Heartkage <p124leo@gmail.com>
2021-01-03 13:40:47 +0800

    Solved Q117

--- NodeProblems.cpp
+++ NodeProblems.cpp
@@ -19,7 +19,7 @@ public:
 
 class Solution {
 private:
-    // Q116
+    // Q116. Populating Next Right Pointers in Each Node
     void RecursiveConnect(Node* left, Node* right)
     {
         if(left == NULL)
@@ -29,17 +29,63 @@ private:
         RecursiveConnect(left->right, (right == NULL) ? NULL : right->left);
     }
 
+    // Q117. Populating Next Right Pointers in Each Node II
+    bool m_foundNextTarge;
+    Node* m_root;
+    Node* RecursiveSearchNext(Node* _current, Node* _target, int _depth, int _maxDepth)
+    {
+        if(_current == NULL)
+            return NULL;
+        else if(_depth >= _maxDepth)
+        {
+            if(_current == _target)
+            {
+                m_foundNextTarge = true;
+                return NULL;
+            }
+            else if(m_foundNextTarge)
+                return _current;
+            else
+                return NULL;
+        }
+        else
+        {
+            Node* temp = RecursiveSearchNext(_current->left, _target, _depth + 1, _maxDepth);
+            return (temp == NULL) ? RecursiveSearchNext(_current->right, _target, _depth + 1, _maxDepth) : temp;
+        }
+    }
+    void RecursiveConnectFromHead(Node* _current, int _depth)
+    {
+        if(_current == NULL)
+            return;
+        m_foundNextTarge = false;
+        _current->next = RecursiveSearchNext(m_root, _current, 0, _depth);
+        RecursiveConnectFromHead(_current->left, _depth + 1);
+        RecursiveConnectFromHead(_current->right, _depth + 1);
+    }
+
 public:
-    // Q116
+    // Q116, Q117
     public:
     Node* connect(Node* root) {
-        RecursiveConnect(root, NULL);
+        m_root = root;
+        RecursiveConnectFromHead(root, 0);
         return root;
     }
+    
 };
 
 int main()
 {
+    Node* main = new Node();
+    Node* left = new Node();
+    Node* right = new Node();
+
+    main->left = left;
+    main->right = right;
+
+    Solution s;
+    s.connect(main);
 
     return 0;
 }
