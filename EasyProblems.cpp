ff858af7b748d8093dd6a93d4c2be93bbb3031d5
Heartkage <p124leo@gmail.com>
2021-01-05 22:53:26 +0800

    Solved 125

--- EasyProblems.cpp
+++ EasyProblems.cpp
@@ -30,14 +30,49 @@ public:
     }
 };
 
+// 125. Valid Palindrome
+class Solution125 {
+private:
+    bool IsAlphabetNumeric(char _s)
+    {
+        if((_s >= 48 && _s <= 57) || (_s >= 65 && _s <= 90) || (_s >= 97 && _s <= 122))
+            return true;
+        else
+            return false;
+    }
+    bool IsSameChar(char _first, char _second)
+    {
+        if(_first >= 65 && _first <= 90)
+            _first += 32;
+        if(_second >= 65 && _second <= 90)
+            _second += 32;
+        return _first == _second;
+    }
+public:
+    bool isPalindrome(string s) {
+        int frontIndex = 0;
+        int backIndex = s.size()-1;
+
+        while(frontIndex <= backIndex)
+        {
+            while((frontIndex <= backIndex) && !IsAlphabetNumeric(s[frontIndex])) frontIndex++;
+            while((frontIndex <= backIndex) && !IsAlphabetNumeric(s[backIndex])) backIndex--;
+
+            if(frontIndex <= backIndex)
+            {
+                if(!IsSameChar(s[frontIndex++], s[backIndex--]))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
+
 int main()
 {
-    Solution solution;
+    Solution125 s;
 
-    vector<int> answers = solution.getRow(5);
-    for(int i = 0; i < answers.size(); i++)
-        cout << answers[i] << " ";
-    cout << endl;
+    s.isPalindrome("A man, a plan, a canal: Panama");
 
     return 0;
 }
