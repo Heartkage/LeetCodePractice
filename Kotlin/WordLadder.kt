42d8d396baf9dd5069f8b6c3e5fc18d248de28a4
Heartkage <p124leo@gmail.com>
2021-01-19 18:37:11 +0800

    Modify Ladder

--- Kotlin/WordLadder.kt
+++ Kotlin/WordLadder.kt
@@ -2,17 +2,16 @@ class Solution {
     inline fun String.addPath(_wordList: List<String>, _availableRoute : MutableMap<String, MutableList<String>>)
     {
         val targetWord :String = this;
-        println("This Word: $targetWord")
+        //println("This Word: $targetWord")
         var routes : MutableList<String> = mutableListOf()
         _availableRoute[targetWord] = routes
-
 
         for(i in 0 until (targetWord.length))
         {
             val regexCharArray = targetWord.toCharArray()
-            regexCharArray[i] = '*'
+            regexCharArray[i] = '.'
             val regexString : String = regexCharArray.concatToString()
-            println("Modified Word: $regexString")
+            //println("Modified Word: $regexString")
             val r1 = Regex(regexString)
 
             _wordList.forEach{
@@ -28,11 +27,41 @@ class Solution {
 
     }
 
+    fun PrintRoutes(availableRoute: MutableMap<String, MutableList<String>>){
+        availableRoute.forEach{
+            println("---Current Word (${it.key}) has ${it.value.count()} routes---")
+            print("[Words]: ")
+            it.value.forEach{
+                print("${it} ")
+            }
+            println()
+            println("--End of This Word--")
+        }
+    }
 
     fun ladderLength(beginWord: String, endWord: String, wordList: List<String>): Int {
         val availableRoute: MutableMap<String, MutableList<String>> = mutableMapOf()
+        val routeTrace : MutableMap<String, Boolean> = mutableMapOf()
+        val routeDistance : MutableMap<String, Int> = mutableMapOf()
 
         beginWord.addPath(wordList, availableRoute);
+        routeTrace[beginWord] = true
+        routeDistance[beginWord] = 0
+        wordList.forEach{
+            it.addPath(wordList, availableRoute)
+            routeTrace[it] = false
+            routeDistance[it] = Int.MAX_VALUE
+        }
+
+        PrintRoutes(availableRoute)
+        var searchList : ArrayList<String> = arrayListOf();
+        searchList.add(beginWord)
+
+
+        while(!searchList.isEmpty())
+        {
+
+        }
 
         return 0
     }
