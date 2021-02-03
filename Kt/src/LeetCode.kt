6cda86079337987642566261275b91fd001fd529
Heartkage <p124leo@gmail.com>
2021-02-03 23:48:13 +0800

    Solved SingleNumber

--- Kt/src/LeetCode.kt
+++ Kt/src/LeetCode.kt
@@ -4,7 +4,20 @@
 }
 
 class Solution {
+    fun singleNumber(nums: IntArray): Int {
+        nums.sort();
 
+        var num = 0
+        for(i in nums.indices step 3)
+        {
+            num = nums[i]
+
+            if((i + 2) < nums.size)
+                if(nums[i] != nums[i + 2])
+                    break
+        }
+        return num
+    }
 }
 
 fun main() {
