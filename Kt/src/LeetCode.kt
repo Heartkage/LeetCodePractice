14650ce3067a2d772840d00d5e6b70dd54299427
Heartkage <p124leo@gmail.com>
2021-02-14 16:56:32 +0800

    Solved Q141

--- Kt/src/LeetCode.kt
+++ Kt/src/LeetCode.kt
@@ -1,13 +1,29 @@
  class TreeNode(var `val`: Int) {
      var left: TreeNode? = null
      var right: TreeNode? = null
-}
+ }
 
-class Solution {
+ class ListNode(var `val`: Int) {
+     var next: ListNode? = null
+ }
 
-}
+ class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+        var tortoise = head
+        var hare = head
+
+        do{
+            tortoise = tortoise?.next
+            hare = hare?.next?.next
+        } while(tortoise != hare)
+
+        return tortoise != null;
+    }
+ }
 
 fun main() {
     val solution : Solution = Solution()
+    val node : ListNode = ListNode(7)
 
+    println(solution.hasCycle(node))
 }
