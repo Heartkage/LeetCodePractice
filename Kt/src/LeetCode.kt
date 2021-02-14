84a54d966a80b1ef8f59f6c88f286a3708cea3bc
Heartkage <p124leo@gmail.com>
2021-02-14 19:02:04 +0800

    Solved Q145

--- Kt/src/LeetCode.kt
+++ Kt/src/LeetCode.kt
@@ -25,9 +25,35 @@ class Solution {
 
         return answers
     }
+
+    fun postorderTraversal(root: TreeNode?): List<Int> {
+        var answers : MutableList<Int> = mutableListOf()
+
+        var nodeStack : Stack<TreeNode> = Stack()
+        nodeStack.push(root)
+
+        while(nodeStack.isNotEmpty())
+        {
+            var current : TreeNode? = nodeStack.pop()
+            if(current != null)
+            {
+                if(current.left == null && current.right == null)
+                    answers.add(current.`val`)
+                else
+                {
+                    nodeStack.add(TreeNode(current.`val`))
+                    if(current.right != null)
+                        nodeStack.push(current.right)
+                    if(current.left != null)
+                        nodeStack.push(current.left)
+                }
+            }
+        }
+        return answers
+    }
 }
 
-fun main() {
     val solution : Solution = Solution()
+fun main() {
 
 }
