57d9029388be176714e1689a86cb461d3b44135b
Heartkage <p124leo@gmail.com>
2021-02-14 18:42:06 +0800

    Solved Q144

--- Kt/src/LeetCode.kt
+++ Kt/src/LeetCode.kt
@@ -1,10 +1,30 @@
- class TreeNode(var `val`: Int) {
+import java.util.*
+
+class TreeNode(var `val`: Int) {
      var left: TreeNode? = null
      var right: TreeNode? = null
 }
 
 class Solution {
+    fun preorderTraversal(root: TreeNode?): List<Int> {
+        var answers : MutableList<Int> = mutableListOf()
 
+        var nodeStack : Stack<TreeNode> = Stack()
+        nodeStack.push(root)
+
+        while(nodeStack.isNotEmpty())
+        {
+            var current : TreeNode? = nodeStack.pop()
+            if(current != null)
+            {
+                answers.add(current.`val`)
+                nodeStack.push(current.right)
+                nodeStack.push(current.left)
+            }
+        }
+
+        return answers
+    }
 }
 
 fun main() {
