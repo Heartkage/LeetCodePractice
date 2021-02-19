e5f41ddea4268ac3200fceef343a57d2c5aaff2a
Heartkage <p124leo@gmail.com>
2021-02-19 23:43:00 +0800

    Solved SubsetII and Combinations

--- C++/SubsetAndCombination/Subsets.cpp
+++ C++/SubsetAndCombination/Subsets.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<vector>
+#include<set>
+#include<algorithm>
 using namespace std;
 
 class Solution {
@@ -20,13 +22,33 @@ public:
         }
         return answer;
     }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> answer;
+        set<vector<int>> sets;
+        answer.push_back(vector<int>());
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int currentSize = answer.size();
+            for(int j = 0; j < currentSize; j++)
+            {
+                vector<int> list(answer[j]);
+                list.push_back(nums[i]);
+                if(sets.insert(list).second)
+                    answer.push_back(list);
+            }
+        }
+        return answer;
+    }
 };
 
 int main(void)
 {
     Solution s;
-    vector<int> list{1, 2, 3};
-    vector<vector<int>> answer = s.subsets(list);
+    vector<int> list{1, 2, 2};
+    vector<vector<int>> answer = s.subsetsWithDup(list);
 
     for(int i = 0; i < answer.size(); i++)
     {
