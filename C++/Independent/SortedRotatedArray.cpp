6139141526e65fbd44ed92de20a79a885bb6dc4b
Heartkage <p124leo@gmail.com>
2021-02-26 22:50:16 +0800

    Solved Q154 rotated Array II

--- C++/Independent/SortedRotatedArray.cpp
+++ C++/Independent/SortedRotatedArray.cpp
@@ -5,20 +5,26 @@ using namespace std;
 class Solution {
     int RecursivelySearchMinimum(vector<int> &nums, int leftIndex, int rightIndex)
     {
-        //cout << "CurrentIndex: " << leftIndex << ", right: " << rightIndex << endl;
+        cout << "CurrentIndex: " << leftIndex << ", right: " << rightIndex << endl;
         if(leftIndex == rightIndex)
             return nums[leftIndex];
 
         int midIndex = (leftIndex + rightIndex) / 2;
 
-        if(nums[midIndex] >= nums[leftIndex] && nums[midIndex] >= nums[rightIndex])
+        if(nums[midIndex] == nums[leftIndex] && nums[midIndex] == nums[rightIndex])
         {
-            //cout << "Choose Range: " << nums[midIndex + 1] << ", " << nums[rightIndex] << endl;
+            int left = RecursivelySearchMinimum(nums, leftIndex, midIndex);
+            int right = RecursivelySearchMinimum(nums, midIndex + 1, rightIndex);
+            return min(left, right);
+        }
+        else if(nums[midIndex] >= nums[leftIndex] && nums[midIndex] > nums[rightIndex])
+        {
+            cout << "Choose Range: " << nums[midIndex + 1] << ", " << nums[rightIndex] << endl;
             return RecursivelySearchMinimum(nums, midIndex + 1, rightIndex);
         }
         else
         {
-            //cout << "Choose Range: " << nums[leftIndex] << ", " << nums[midIndex] << endl;
+            cout << "Choose Range: " << nums[leftIndex] << ", " << nums[midIndex] << endl;
             return RecursivelySearchMinimum(nums, leftIndex, midIndex);
         }
     }
@@ -31,7 +37,7 @@ public:
 int main(void)
 {
     Solution s;
-    vector<int> list{0,1,2,3,4,5,6};
+    vector<int> list{3,3,1,3};
     cout << "Answer: " << s.findMin(list) << endl;
     return 0;
 }
