df34534527c2225a0cfdb8a036ebe83f8344fc9c
Heartkage <p124leo@gmail.com>
2021-02-27 11:05:09 +0800

    Solved Q33

--- C++/Independent/SortedRotatedArray.cpp
+++ C++/Independent/SortedRotatedArray.cpp
@@ -28,16 +28,63 @@ class Solution {
             return RecursivelySearchMinimum(nums, leftIndex, midIndex);
         }
     }
+
+    int RecursivelySearchTarget(vector<int> &nums, int _target, int leftIndex, int rightIndex)
+    {
+        cout << "CurrentIndex: " << leftIndex << ", right: " << rightIndex << endl;
+        if(leftIndex == rightIndex)
+        {
+            if(nums[leftIndex] == _target)
+                return leftIndex;
+            else
+                return -1;
+        }
+        
+        int midIndex = (leftIndex + rightIndex) / 2;
+
+        if(nums[midIndex] >= nums[leftIndex] && nums[midIndex] > nums[rightIndex])
+        {
+            if(nums[leftIndex] <= _target && _target <= nums[midIndex])
+            {
+                cout << "Choose Range: " << nums[leftIndex] << ", " << nums[midIndex] << endl;
+                return RecursivelySearchTarget(nums, _target, leftIndex, midIndex);
+            }
+            else 
+            {
+                cout << "Choose Range: " << nums[midIndex + 1] << ", " << nums[rightIndex] << endl;
+                return RecursivelySearchTarget(nums, _target, midIndex + 1, rightIndex);
+            }
+        }
+        else
+        {
+            if(nums[midIndex] < _target && _target <= nums[rightIndex])
+            {
+                cout << "Choose Range: " << nums[midIndex + 1] << ", " << nums[rightIndex] << endl;
+                return RecursivelySearchTarget(nums, _target, midIndex + 1, rightIndex);
+            }
+            else
+            {
+                cout << "Choose Range: " << nums[leftIndex] << ", " << nums[midIndex] << endl;
+                return RecursivelySearchTarget(nums, _target, leftIndex, midIndex);
+            }
+        }
+    }
 public:
     int findMin(vector<int>& nums) {
         return RecursivelySearchMinimum(nums, 0, nums.size()-1);
+    }
+    int search(vector<int>& nums, int target) {
+        return RecursivelySearchTarget(nums, target, 0, nums.size()-1);
     }
 };
 
 int main(void)
 {
     Solution s;
-    vector<int> list{3,3,1,3};
-    cout << "Answer: " << s.findMin(list) << endl;
+    vector<int> list{4,5,6,7,8,1,2,3};
+    for(int i = 0; i < list.size(); i++)
+        cout << list[i] << " ";
+    cout << endl;
+    cout << "Answer: " << s.search(list, 5) << endl;
     return 0;
 }
