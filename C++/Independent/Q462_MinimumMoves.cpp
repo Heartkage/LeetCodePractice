e0fb70f3b79d235f1b3bca5f9daf42cd926e942b
LeoChien <leochien@243studios.com>
2022-06-30 15:04:59 +0800

    Solved Q462

---
+++ C++/Independent/Q462_MinimumMoves.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int median = nums[nums.size()/2];
+        int answer = 0;
+        for(int i = 0; i < nums.size(); i++)
+            answer += abs(nums[i] - median);
+        return answer;     
+    }
+};
+
+
+int main(void)
+{
+    vector<int> input;
+    input.push_back(1);
+    input.push_back(0);
+    input.push_back(0);
+    input.push_back(8);
+    input.push_back(6);
+
+    Solution s;
+    cout << s.minMoves2(input) << endl;
+
+    return 0;
+}
