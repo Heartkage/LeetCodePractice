923a3509e8c34a713bfcfbb27a31f899654f038c
LeoChien <p124leo@gmail.com>
2023-01-04 12:05:51 +0800

    [Modified] Q2244 (Math, 74.17% / 45.14%)

--- C++/Independent/Q2244_MinimumRoundstoCompleteAllTasks.cpp
+++ C++/Independent/Q2244_MinimumRoundstoCompleteAllTasks.cpp
@@ -1,48 +1,26 @@
 #include<iostream>
 #include<vector>
 #include<unordered_map>
-#include<map>
 using namespace std;
 
 class Solution {
-    int highestSeen;
-    unordered_map<int, int> seenNum;
-    int RecursiveCount(int n){
-        if(n == 2 || n == 3)
-            return 1;
-        else if(seenNum.find(n) != seenNum.end())
-            return seenNum[n];
-        else{
-            int left = highestSeen;
-            int right = n - left;
-            int leftCount = RecursiveCount(left);
-            seenNum.insert(pair<int, int>(left, leftCount));
-            highestSeen = max(highestSeen, leftCount);
-            int rightCount = RecursiveCount(right);
-            seenNum.insert(pair<int, int>(right, rightCount));
-            highestSeen = max(highestSeen, rightCount);
-            return leftCount + rightCount;
-        }
-    }
 public:
     int minimumRounds(vector<int>& tasks) {
         int totalRounds = 0;
-        map<int, int> table;
+        unordered_map<int, int> table;
         for(int i = 0; i < tasks.size(); i++){
             if(table.find(tasks[i]) != table.end())
                 table[tasks[i]]++;
             else
                 table.insert(pair<int, int>(tasks[i], 1));
         }
-        map<int, int>::iterator it = table.begin();
-        if(it->second > 4)
-            highestSeen = 3;
-        else
-            highestSeen = 2;
+        unordered_map<int, int>::iterator it = table.begin();
         while(it != table.end()){
-            if(it->second < 2)
+            int count = it->second;
+            if(count < 2)
                 return -1;
-            totalRounds += RecursiveCount(it->second);
+            else
+                totalRounds += ceil(count / 3.0);
             it++;
         }
         return totalRounds;
@@ -56,6 +34,7 @@ int main(void){
     tasks.push_back(3);
     tasks.push_back(3);
     tasks.push_back(2);
+    tasks.push_back(4);
     tasks.push_back(4);
 
     Solution s;
