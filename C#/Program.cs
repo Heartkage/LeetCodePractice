a24068d948750eccfabe2fe1bb873d39af6b978b
LeoChien <p124leo@gmail.com>
2023-01-22 13:45:16 +0800

    [Solved][C#] Q131 (Recursion, 40.28% / 71.53%)

--- C#/Program.cs
+++ C#/Program.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using Uitilities;
 using Uitilities.Number;
 
+/*
+    cd to C# folder -> dotnet run(in terminal)
+*/
+
 namespace CSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Solved131();
+        }
+
+        static void Solve100(){
             TestTree test = new TestTree();
             var heads = test.GenerateTestCase(2, 0);
             for(int i = 0; i < heads.Count; i++){
@@ -16,6 +24,17 @@ namespace CSharp
             }
             Solution100 s = new Solution100();
             Console.WriteLine("Ans: " + s.IsSameTree(heads[0], heads[1]));
+        }
+    
+        static void Solved131(){
+            Solution131 s = new Solution131();
+            var res = s.Partition("aab");
+            foreach(var list in res){
+                foreach(var innerList in list){
+                    Console.WriteLine(innerList);
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
