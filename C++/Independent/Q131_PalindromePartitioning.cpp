b7268fb070a449140d5e2a6aee15a7a2800686b7
LeoChien <p124leo@gmail.com>
2023-01-22 13:44:37 +0800

    [Update] Q131 (Recursion, 96.33% / 80.30%)

--- C++/Independent/Q131_PalindromePartitioning.cpp
+++ C++/Independent/Q131_PalindromePartitioning.cpp
@@ -4,26 +4,27 @@
 using namespace std;
 
 class Solution {
-    vector<vector<string> > answers;
-    vector<string> currentAnswer;
-
-    bool IsPalindrome(string s){
-        string temp = s;
-        reverse(temp.begin(), temp.end());
-        return temp.compare(s) == 0;
+    bool IsPalindrome(string &s){
+        int j = s.length() - 1;
+        for(int i = 0; i < j; i++, j--){
+            if(s[i] != s[j])
+                return false;
+        }
+        return true;
     }
 
-    void RecursiveSearch(string s){
-        if(s.length() == 0){
+    void RecursiveSearch(int index, const string &s, vector<vector<string> > &answers, vector<string> &currentAnswer){
+        int n = s.length();
+        if(index == n){
             answers.push_back(currentAnswer);
             return;
         }
 
-        for(int i = 0; i < s.length(); i++){
-            string frontString = s.substr(0, i+1);
+        for(int i = index; i < n; i++){
+            string frontString = s.substr(index, i-index+1);
             if(IsPalindrome(frontString)){
                 currentAnswer.push_back(frontString);
-                RecursiveSearch(s.substr(i+1));
+                RecursiveSearch(i+1, s, answers, currentAnswer);
                 currentAnswer.pop_back();
             }
         }
@@ -31,8 +32,9 @@ class Solution {
 
 public:
     vector<vector<string> > partition(string s) {
-        answers.clear();
-        RecursiveSearch(s);
+        vector<vector<string> > answers;
+        vector<string> currentAnswer;
+        RecursiveSearch(0, s, answers, currentAnswer);
         return answers;
     }
 };
@@ -40,7 +42,7 @@ public:
 int main(void){
 
     Solution s;
-    vector<vector<string> > answer = s.partition("aaaaaaaaaa");
+    vector<vector<string> > answer = s.partition("aab");
     cout << "answers: " << endl;
     for(int i = 0; i < answer.size(); i++){
         for(int j = 0; j < answer[i].size(); j++){
