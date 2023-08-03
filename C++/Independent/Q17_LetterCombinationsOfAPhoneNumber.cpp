02678631cc574d679192f0063895fd8a5e58c9c7
LeoChien <p124leo@gmail.com>
2023-08-03 10:26:57 +0800

    [Update] Q17 (recursion, 100% / 55.27%) [O(4 ^ n) / O(4 ^ n)]

--- C++/Independent/Q17_LetterCombinationsOfAPhoneNumber.cpp
+++ C++/Independent/Q17_LetterCombinationsOfAPhoneNumber.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 class Solution {
-public:
-    vector<string> letterCombinations(string digits) {
+    vector<string> UsingCalculation(string digits){
         int total = 0;
         int n = digits.size();
         if(n > 0){
@@ -43,11 +42,40 @@ public:
         }
         return ans;
     }
+
+    void RecursiveSearch(vector<string> &ans, string &current, string &digits, int index){
+        if(digits.size() == index){
+            ans.push_back(current);
+        }
+        else{
+            short digit = digits[index] - 48;
+            short characterCount = (digit < 9 && digit != 7) ? 3 : 4;
+            char startCharater = (digit < 8) ? 'a' : 't';
+            short startingIndex = (digit < 8) ? 2 : 8;
+
+            for(int i = 0; i < characterCount; i++){
+                current.push_back(startCharater + (digit-startingIndex) * 3 + i);
+                RecursiveSearch(ans, current, digits, index + 1);
+                current.pop_back();
+            }
+        }
+    }
+    vector<string> RecursiveMethod(string digits){
+        vector<string> ans;
+        string current;
+        if(digits.size() > 0)
+            RecursiveSearch(ans, current, digits, 0);
+        return ans;
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        return RecursiveMethod(digits);
+    }
 };
 
 int main(void){
     Solution s;
-    vector<string> ans = s.letterCombinations("");
+    vector<string> ans = s.letterCombinations("9487");
     for(int i = 0; i < ans.size(); i++){
         cout << ans[i] << endl;
     }
