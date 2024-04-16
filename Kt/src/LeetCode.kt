810689cfe51aac75b7ccea4e194920bd7df3db6c
LeoChien <p124leo@gmail.com>
2024-04-16 16:00:43 +0800

    [Re-upload] Solved Q129, recursion

--- Kt/src/LeetCode.kt
+++ Kt/src/LeetCode.kt
@@ -4,26 +4,9 @@ class TreeNode(var `val`: Int) {
 }
 
 class Solution {
-    fun sumNumbers(root: TreeNode?): Int {
-        return RecursiveAddNumber(root, 0)
-    }
-
-    fun RecursiveAddNumber(node: TreeNode?, parentValue: Int): Int{
-        return if(node == null)
-            0
-        else if(node.left == null && node.right == null)
-            node.`val` + parentValue*10
-        else {
-            var answer = 0
-            val nextValue = parentValue*10 + node.`val`
-            answer += RecursiveAddNumber(node.left, nextValue)
-            answer += RecursiveAddNumber(node.right, nextValue)
-            answer
-        }
-    }
+    
 }
 
 fun main() {
-    val solution : Solution
 
 }
