d2e84749b153b43ac1a526d3f15567e4029e20a2
Heartkage <p124leo@gmail.com>
2025-01-11 17:12:57 +0800

    [Solved] Q1400 (table, 62.59% / 51.75%) [O(n)/ O(1)]

---
+++ C++/Independent/Q1400_ConstructKPalindromeStrings.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        int n = s.length();
+        if(k > n) 
+            return false;
+
+        vector<int> table(26, 0);
+        for(int i = 0; i<n; i++) {
+            table[s[i]-'a']++;
+        }
+        
+        int minimumK = 0;
+        int maximumK = 0;
+        for(int i = 0; i < 26; i++) {
+            maximumK += table[i];
+            if(table[i] & 1) {
+                minimumK++;
+                maximumK++;
+            }
+        }
+        
+        return k >= minimumK && k <= maximumK;
+    }
+};
+
+int main(void) {
+    Solution s;
+    string a = "annabelle";
+    int k = 10;
+    for(int i = 1; i<=k; i++) {
+        cout << "s: " << a << ", k: " << i << " = " << s.canConstruct(a, i) << endl;
+    }
+    return 0;
+}
